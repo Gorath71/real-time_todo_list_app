58f135f4ece1bc05dd104f2f3ab6a41e512574a3
Gorath71 <Programming.r24pP@pm.me>
2025-06-24 18:10:10 +0200

    add: basic TaskList controler setup

---
+++ backend/src/main/java/com/project/tutorial/controler/TaskListControler.java
@@ -0,0 +1,39 @@
+package com.project.tutorial.controler;
+
+import com.project.tutorial.model.TaskList;
+import com.project.tutorial.service.TaskListService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasklist")
+public class TaskListControler {
+    private final TaskListService taskListService;
+
+    public TaskListControler(TaskListService taskListService) {
+        this.taskListService = taskListService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskList> getTaskList(@PathVariable Long id) {
+        TaskList taskList = taskListService.getTaskListById(id);
+        if (taskList == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(taskList);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskList>> getAllTaskList() {
+        List<TaskList> taskLists = taskListService.getAllTaskLists();
+        return ResponseEntity.ok(taskLists);
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskList> createTaskList(@RequestBody TaskList taskList) {
+        TaskList newTaskList = taskListService.createTaskList(taskList);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTaskList);
+    }
+}
