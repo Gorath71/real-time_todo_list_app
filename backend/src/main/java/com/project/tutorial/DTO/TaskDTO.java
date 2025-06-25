91ec4fdc4a41f90477d84c73bb738e2a36eba6e6
Gorath71 <Programming.r24pP@pm.me>
2025-06-25 16:28:23 +0200

    add: task dto

---
+++ backend/src/main/java/com/project/tutorial/DTO/TaskDTO.java
@@ -0,0 +1,23 @@
+package com.project.tutorial.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TaskDTO {
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private boolean completed;
+
+    @NotNull
+    private Long taskListId;
+}
