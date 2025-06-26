9ae41ce20558937995b241dedbc3f055c24e1c89
Gorath71 <Programming.r24pP@pm.me>
2025-06-26 14:02:09 +0200

    add: task controller mapping

--- backend/src/main/java/com/project/tutorial/controller/TaskController.java
+++ backend/src/main/java/com/project/tutorial/controller/TaskController.java
@@ -1,12 +1,12 @@
 package com.project.tutorial.controller;
 
+import com.project.tutorial.DTO.TaskDTO;
 import com.project.tutorial.model.Task;
 import com.project.tutorial.service.TaskService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/task")
@@ -19,6 +19,32 @@ public class TaskController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTask(@PathVariable Long id) {
-
+        Task task = taskService.getTaskById(id);
+        return ResponseEntity.ok(task);
     }
+
+    @GetMapping("/tasklist/{id}")
+    public ResponseEntity<List<Task>> getAllTasksByTaskListId(@PathVariable Long id) {
+        List<Task> tasks = taskService.getAllTasksByTaskListId(id);
+        return ResponseEntity.ok(tasks);
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody TaskDTO taskDTO) {
+        Task task = taskService.createTask(taskDTO);
+        return ResponseEntity.ok(task);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
+        Task task = taskService.updateTask(id, taskDTO);
+        return ResponseEntity.ok(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Task> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
